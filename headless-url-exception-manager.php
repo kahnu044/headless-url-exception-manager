86256dbde5e18140c8fcdd4fa056b341d6cf530b
kahnu044 <kahnucharan044@gmail.com>
2025-01-29 14:45:55 +0530

    plugin page added

---
+++ headless-url-exception-manager.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Plugin Name: Headless URL Exception Manager
+ * Description: Allows users to input URLs that should be ignored from redirection in Headless Mode.
+ * Version: 1.0.0
+ * Author: kahnu044
+ */
